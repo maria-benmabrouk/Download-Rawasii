b60cc426135087e0a015c7eb09878aca7aad5fa3
Mariya <benmabroukmariya@gmail.com>
2026-05-13 00:11:39 +0100

    commit13

--- project.js
+++ project.js
@@ -6,3 +6,7 @@ function showMessage2(system) {
   document.getElementById("msg2").innerText =
   `${system} version coming soon`;
 }
+function showMessage1(system) {
+  document.getElementById("msg1").innerText =
+  `${system} version coming soon`;
+}

9006a4a31524655994d8859fa2000b456201f462
Mariya <benmabroukmariya@gmail.com>
2026-05-12 23:45:52 +0100

    commit12

--- project.js
+++ project.js
@@ -2,3 +2,7 @@ function showMessage(system) {
   document.getElementById("msg").innerText =
   `${system} version coming soon`;
 }
+function showMessage2(system) {
+  document.getElementById("msg2").innerText =
+  `${system} version coming soon`;
+}
